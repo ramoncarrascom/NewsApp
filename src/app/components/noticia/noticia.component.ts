8b48b5cfd855ea4f1b891349303d9af0ae32a6d1
ramoncarrascom <ramon@ramoncarrasco.es>
2020-10-28 20:48:07 +0100

    ADDED open news in browser

--- src/app/components/noticia/noticia.component.ts
+++ src/app/components/noticia/noticia.component.ts
@@ -1,6 +1,8 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Article } from 'src/app/interfaces/interfaces';
 
+import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
+
 @Component({
   selector: 'app-noticia',
   templateUrl: './noticia.component.html',
@@ -11,8 +13,12 @@ export class NoticiaComponent implements OnInit {
   @Input() noticia: Article;
   @Input() indice: number = 0;
 
-  constructor() { }
+  constructor( private iab: InAppBrowser ) { }
 
   ngOnInit() {}
 
+  abrirNoticia() {
+    console.log('Noticia', this.noticia.url);
+    const browser = this.iab.create(this.noticia.url, '_system');
+  }
 }
